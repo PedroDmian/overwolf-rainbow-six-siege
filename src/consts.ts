32807b302a911909edf5856be0d239fd9c6dbc80
Damian Palomo <pedro_dmian@hotmail.com>
2021-05-24 18:02:47 -0500

    Init commit

---
+++ src/consts.ts
@@ -0,0 +1,29 @@
+const GameClassId = 10826;
+
+const interestingFeatures = [
+  'gep_internal',
+  'game_info',
+  'match',
+  'match_info',
+  'roster',
+  'kill',
+  'death',
+  'me',
+  'defuser'
+];
+
+const windowNames = {
+  inGame: 'in_game',
+  desktop: 'desktop'
+};
+
+const hotkeys = {
+  toggle: 'showhide'
+};
+
+export {
+  GameClassId,
+  interestingFeatures,
+  windowNames,
+  hotkeys
+}
